ea049597257dd614702a9c4211c94da8a6ab353b
Veronicah Kanja <kanjaveronicah@gmail.com>
2025-09-22 15:59:47 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record) {
+    const win = record.find(game=> game.result === "W");
+    if (win) {
+        return win.year;
+    }
+    return undefined
+}
